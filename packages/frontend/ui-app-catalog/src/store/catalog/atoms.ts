df80698df6feeeffde6f519c6f50cf9e1d63844d
Akash-M <akash.manjunath@gmail.com>
2021-11-23 20:18:06 +0100

    refactor: remove unused paginator state

--- packages/frontend/ui-app-catalog/src/store/catalog/atoms.ts
+++ packages/frontend/ui-app-catalog/src/store/catalog/atoms.ts
@@ -4,15 +4,3 @@ export const AppListState = atom<AppCatalog.Catalog[]>({
   key: 'CatalogList',
   default: [],
 });
-
-export interface AppListPaginator {
-  perPage: number;
-  currentPage: number;
-}
-export const AppListPaginatorState = atom<AppListPaginator>({
-  key: 'AppListPaginator',
-  default: {
-    currentPage: 1,
-    perPage: 2,
-  },
-});
