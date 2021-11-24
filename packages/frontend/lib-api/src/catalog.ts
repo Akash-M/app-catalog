c02c39b5f855f5074bab2c4a0437d5507c5a0cc6
Akash-M <akash.manjunath@gmail.com>
2021-11-24 21:46:37 +0100

    feat: display selected app details along with rendering readme information

--- packages/frontend/lib-api/src/catalog.ts
+++ packages/frontend/lib-api/src/catalog.ts
@@ -1,4 +1,4 @@
-import { appCatalogApi } from './axios';
+import { appCatalogApi, externalApi } from './axios';
 
 export async function getApps(): Promise<AppCatalog.Catalog[]> {
   return (await appCatalogApi.get('/apps')).data;
@@ -6,4 +6,8 @@ export async function getApps(): Promise<AppCatalog.Catalog[]> {
 
 export async function getAppById(id: string): Promise<AppCatalog.Catalog> {
   return (await appCatalogApi.get(`/apps/${id}`)).data;
+}
+
+export async function getAppReadme(url: string): Promise<string> {
+  return (await externalApi.get(url)).data;
 }
