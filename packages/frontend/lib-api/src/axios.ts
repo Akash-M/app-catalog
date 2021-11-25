45f369b8b37574684f60a18b62a89834d9528387
Akash-M <akash.manjunath@gmail.com>
2021-11-25 18:12:22 +0100

    chore: add tsdocs to lib-api

--- packages/frontend/lib-api/src/axios.ts
+++ packages/frontend/lib-api/src/axios.ts
@@ -6,9 +6,11 @@ const requestConfig: AxiosRequestConfig = {
 
 const BASE_URL = 'http://localhost:3000';
 
+// Axios config to connect with backend.
 export const appCatalogApi = axios.create({
   ...requestConfig,
   baseURL: BASE_URL,
 });
 
+// Axios config for making external api calls like fetching readmes.
 export const externalApi = axios.create({ ...requestConfig });
--- packages/frontend/lib-api/src/catalog.ts
+++ packages/frontend/lib-api/src/catalog.ts
@@ -1,13 +1,29 @@
 import { appCatalogApi, externalApi } from './axios';
 
+/**
+ * GET request to fetch list of all applications.
+ *
+ * @return Promise<AppCatalog.Catalog[]>
+ */
 export async function getApps(): Promise<AppCatalog.Catalog[]> {
   return (await appCatalogApi.get('/apps')).data;
 }
 
+/**
+ * GET request to fetch application information based on id.
+ *
+ * @param id: App id to be used to fetch detailed information.
+ * @return Promise<AppCatalog.Catalog>
+ */
 export async function getAppById(id: string): Promise<AppCatalog.Catalog> {
   return (await appCatalogApi.get(`/apps/${id}`)).data;
 }
 
+/**
+ * GET request to fetch data from external urls like ReadMes.
+ *
+ * @param url: external url from which to fetch readme information.
+ */
 export async function getAppReadme(url: string): Promise<string> {
   return (await externalApi.get(url)).data;
 }
